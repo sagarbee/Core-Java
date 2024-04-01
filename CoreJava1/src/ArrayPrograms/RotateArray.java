7d5b81336e71bc054f01a134e50c8fb49d11f24f
sagar <sagarmore1752000@gmail.com>
2024-04-01 19:23:41 +0530

    Rotate Array Left and Right

--- CoreJava1/src/ArrayPrograms/RotateArray.java
+++ CoreJava1/src/ArrayPrograms/RotateArray.java
@@ -1,5 +1,43 @@
 package ArrayPrograms;
 
+import java.util.Arrays;
+
 public class RotateArray {
 
+	public static void main(String[] args) {
+
+		int[] arr = { 1, 2, 3, 4, 5, 6 };
+		int[] arr1 = { 1, 2, 3, 4, 5, 6 };
+		int[] rotateArrayLeft = rotateLeft(arr, 2);
+		System.out.println(Arrays.toString(rotateArrayLeft));
+		
+		int[] rotateArrayRight = rotateLeft(arr1, 2);
+		System.out.println(Arrays.toString(rotateArrayRight));
+		
+	}
+
+	
+	
+	public static int[] rotateRight(int[] arr, int rotate) {
+		for (int i = 0; i < rotate; i++) {
+			int temp = arr[0];
+			for (int j = 0; j < arr.length; j++) {
+				arr[j+1] = arr[j];
+			}
+			arr[arr.length-1] = temp;
+		}
+		return arr;
+	}
+	
+	
+	public static int[] rotateLeft(int[] arr, int rotate) {
+		for (int i = 0; i < rotate; i++) {
+			int temp = arr[0];
+			for (int j = 0; j<arr.length-1; j++) {
+				arr[j] = arr[j + 1];
+			}
+			arr[arr.length-1] = temp;
+		}
+		return arr;
+	}
 }
