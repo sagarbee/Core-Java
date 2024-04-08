e1bcdcf4eb2f58522ed629fea8ab483aeb0ce323
sagar <sagarmore1752000@gmail.com>
2024-04-09 01:54:32 +0530

    ArrayList Operations
    1. sort list
    2. remove duplicate
    3.list to set
    4.set to list

--- CoreJava3/src/collectionInterrface/CollectionInterface1.java
+++ CoreJava3/src/collectionInterrface/CollectionInterface1.java
@@ -3,7 +3,7 @@ package collectionInterrface;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class CollectionInterface1 {
+public class CollectionInterface1 implements Comparable<CollectionInterface1>{
 
 	String name;
 	int age;
@@ -16,13 +16,41 @@ public class CollectionInterface1 {
 	}
 	
 	public CollectionInterface1(String name,int age, long phone,double salary){
-		
-		
+
 		this.name = name;
 		this.age = age;
 		this.phone = phone;
 		this.salary = salary;
 		
 	}
- 
+
+	public int compareTo(CollectionInterface1 o) {
+		
+		if(this.salary > o.salary)
+		{
+			return 1;
+		}
+		else if(this.salary<o.salary)
+		{
+			return -1;
+		}
+		
+		return 0;
+	}
+	
+	public String toString()
+	{
+		return "[Name: "+name+", Age: "+age+", Phone: "+phone+", Salary: "+salary+"]";
+	}
+	
+	public boolean equals(Object o)
+	{
+		CollectionInterface1 ci = (CollectionInterface1)o;
+		return this.salary == ci.salary;
+	}
+
+	public int hashCode()
+	{
+		return (int)salary;
+	}
 }
--- CoreJava3/src/collectionInterrface/CollectionInterface1Driver.java
+++ CoreJava3/src/collectionInterrface/CollectionInterface1Driver.java
@@ -1,20 +1,67 @@
 package collectionInterrface;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.stream.Collectors;
 
 public class CollectionInterface1Driver {
 	
 	public static void main(String[] args) {
 
-		Collection collection = new ArrayList();
+		Collection<CollectionInterface1> collection = new ArrayList();
 		
+		collection.add(new CollectionInterface1("Sagar",24,8409049147l,450000.00));
+		collection.add(new CollectionInterface1("Sagar",24,8409049147l,450000.00));
+		collection.add(new CollectionInterface1("Sagar",24,8409049147l,450000.00));
 		collection.add(new CollectionInterface1("Sagar",24,8409049147l,450000.00));
 		collection.add(new CollectionInterface1("Rakesh",26,9874549147l,780000.00));
 		collection.add(new CollectionInterface1("Akash",24,7899049147l,960000.00));
 		collection.add(new CollectionInterface1("Adesh",23,978549147l,780000.00));
 		collection.add(new CollectionInterface1("Aditya",22,8796049147l,50000.00));
 		collection.add(new CollectionInterface1("Ramesh",18,89888049147l,850000.00));
+		
+		//downcasted to Arraylist and sort using comparable->compareTo abstract method
+		Collections.sort((ArrayList)collection);
+		
+		Collections.reverse((ArrayList)collection);
+		
+		for(CollectionInterface1 b:collection)
+		{
+			System.out.println(b);
+		}
+		
+		System.out.println("REMOVING DUPLICATES");
+		System.out.println("-------------------------------------------------------------");
+		/*
+		 * We need to override equals and hashCode method to make forEach understand what we are compare to i. salary
+		 */
+		System.out.println("1. using stream distinct forEach");
+		collection.stream().distinct().forEach(obj->System.out.println(obj));
+
+		System.out.println("2. using stream distinct collect");
+		collection.stream().distinct().collect(Collectors.toList()).forEach(obj-> System.out.println(obj));
+		
+		System.out.println("3.Using set ");
+		/*
+		 * Overriding hashCode and equals method is must to convert set <-> list
+		 */
+		HashSet set = new HashSet(collection);
+		/*
+		 * We can't iterate over set as it does not have indexing 
+		 * for(CollectionInterface1 s: (CollectionInterface1)set)
+			{
 			
-		System.out.println();
+			}
+		
+		 */
+		
+		//Convert back to list
+		ArrayList<CollectionInterface1> distinctList = new ArrayList(set);
+		for(CollectionInterface1 ci:distinctList)
+		{
+			System.out.println(ci);
+		}
+		
 	}
 }
