66ffd8fd3652163d7e11112ab12bc9e120e35f46
sagar <sagarmore1752000@gmail.com>
2024-04-11 23:56:18 +0530

    set different priority
    event though different priorities are set they are not that much reliable as sometimes MIN_PRIORITY get executed first and sometimes MAX_PRIORITY

--- Java8Updates/src/multithreading/SumProductThreadDriver.java
+++ Java8Updates/src/multithreading/SumProductThreadDriver.java
@@ -7,6 +7,9 @@ public class SumProductThreadDriver {
 		SumOfDigitThread sum = new SumOfDigitThread();
 		ProductOfDigitThread product = new ProductOfDigitThread();
 		
+		sum.setPriority(Thread.MIN_PRIORITY);
+		product.setPriority(Thread.MAX_PRIORITY);
+		
 		sum.start();
 		product.start();
 		
